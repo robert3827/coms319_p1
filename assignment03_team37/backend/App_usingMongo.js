59ac014555496bc169d587adaa56623ab04094ee
robert3827 <32315287+robert3827@users.noreply.github.com>
2023-12-06 10:39:21 -0600

    add activity03_team37 and get methods working

---
+++ assignment03_team37/backend/App_usingMongo.js
@@ -0,0 +1,47 @@
+var express = require("express");
+var cors = require("cors");
+var app = express();
+var fs = require("fs");
+var bodyParser = require("body-parser");
+app.use(cors());
+app.use(bodyParser.json());
+const port = "8081";
+const host = "localhost";
+app.listen(port, () => {
+    console.log("App listening at http://%s:%s", host, port);
+});
+
+const { MongoClient } = require("mongodb");
+const url = "mongodb://127.0.0.1:27017";
+const dbName ="assignment03_team37"
+const client = new MongoClient(url);
+const db = client.db(dbName);
+
+app.get("/get", async (req, res) => {
+    console.log("App Get Called");
+    await client.connect();
+    console.log("Node connected successfully to GET MongoDB");
+    const query = {};
+    const results = await db
+        .collection("fakestore")
+        .find(query)
+        .limit(100)
+        .toArray();
+    console.log(results);
+    res.status(200);
+    res.send(results);
+});
+
+app.get("/getFromId/:id", async (req, res) => {
+    const robotid = Number(req.params.id);
+    console.log("Robot to find :", robotid);
+    await client.connect();
+    console.log("Node connected successfully to GET-id MongoDB");
+    const query = {"id": robotid };
+    const results = await db.collection("robots")
+    .findOne(query);
+    console.log("Results :", results);
+    if (!results) res.send("Not Found").status(404);
+    else res.send(results).status(200);
+    });
+    
