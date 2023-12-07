9f9a56c4da8a068f555458ce4a39231e415318b4
breckinb <breckinb@iastate.edu>
2023-12-07 16:25:04 -0600

    Add POST

--- assignment03_team37/backend/App_usingMongo.js
+++ assignment03_team37/backend/App_usingMongo.js
@@ -38,10 +38,45 @@ app.get("/getFromId/:id", async (req, res) => {
     await client.connect();
     console.log("Node connected successfully to GET-id MongoDB");
     const query = {"id": robotid };
-    const results = await db.collection("robots")
+    const results = await db.collection("fakestore")
     .findOne(query);
     console.log("Results :", results);
     if (!results) res.send("Not Found").status(404);
     else res.send(results).status(200);
     });
+
+app.post("/addProduct", async (req, res) => {
+    console.log("Adding a product");
+    await client.connect();
+    const values = Object.values(req.body);
+    const id = values[0]; //ID
+    const title = values[1]; //TITLE
+    const price = values[2];
+    const description = values[3];
+    const category = values[4];
+    const image = values[5]; //Maybe change this to reference the locally stored image
+    const rating = values[6]; //ratings object
+    const ratingsValues = Object.values(rating);
+    const rate = ratingsValues[0];
+    const count = ratingsValues[1];
+
+    const ratings = {
+        "rate" : rate,
+        "count" : count
+    }
+
+    const productFormat = {
+        "id": id,
+        "title": title,
+        "price": price,
+        "description": description,
+        "category": category,
+        "image": image,
+        "rating": ratings
+    }
+
+    const results = await db.collection("fakestore").insertOne(productFormat);
+    res.status(200);
+    res.send(results);
+})
     
