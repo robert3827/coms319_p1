d73592f1c6597095aa8eae7ac9dcf1cb467da863
breckinb <breckinb@iastate.edu>
2023-12-10 14:35:29 -0600

    form done

--- assignment03_team37/frontend/src/AppFrontend_routing.js
+++ assignment03_team37/frontend/src/AppFrontend_routing.js
@@ -30,7 +30,6 @@ function App() {
 
     // new Product
     const [addNewProduct, setAddNewProduct] = useState({
-<<<<<<< HEAD
         "id": 0,
         "title": "",
         "price": 0.0,
@@ -38,18 +37,6 @@ function App() {
         "category": "",
         "image": "",
         "rating": 0.0,
-=======
-        id: 0,
-        title: ""
-        ,
-        price: 0.0,
-        description: ""
-        ,
-        category: ""
-        ,
-        image: url + "images/diglett.png",
-        rating: 0.0,
->>>>>>> 028bd7249404ae8c652bc0d3b70574c0a09a7e7f
     });
 
 
@@ -120,27 +107,6 @@ function App() {
     
 
 
-        function handleOnSubmit(e) {
-            e.preventDefault();
-            console.log(e.target.value);
-            fetch(url + "create", {
-                method: "POST",
-                headers: { "Content-Type": "application/json" },
-                body: JSON.stringify(addNewProduct),
-            })
-                .then((response) => response.json())
-                .then((data) => {
-                    console.log("Post a new product completed");
-                    console.log(data);
-                    if (data) {
-                        //const keys = Object.keys(data);
-                        const value = Object.values(data);
-                        alert(value);
-                    }
-                });
-        }
-
-<<<<<<< HEAD
     function postProduct(e) {
         e.preventDefault();
         console.log(e.target.value);
@@ -160,23 +126,6 @@ function App() {
             });
     }
 
-    function handleChange(evt) {
-        const value = evt.target.value;
-        if (evt.target.name === "id") {
-            setAddNewProduct({ ...addNewProduct, id: value });
-        } else if (evt.target.name === "title") {
-            setAddNewProduct({ ...addNewProduct, title: value });
-        } else if (evt.target.name === "price") {
-            setAddNewProduct({ ...addNewProduct, price: value });
-        } else if (evt.target.name === "description") {
-            setAddNewProduct({ ...addNewProduct, description: value });
-        } else if (evt.target.name === "category") {
-            setAddNewProduct({ ...addNewProduct, category: value });
-        } else if (evt.target.name === "image") {
-            setAddNewProduct({ ...addNewProduct, image: value });
-        } else if (evt.target.name === "rating") {
-            setAddNewProduct({ ...addNewProduct, rating: value });
-=======
         function handleChange(evt) {
             const value = evt.target.value;
             if (evt.target.name === "id") {
@@ -194,56 +143,8 @@ function App() {
             } else if (evt.target.name === "rating") {
                 setAddNewProduct({ ...addNewProduct, rating: value });
             }
->>>>>>> 028bd7249404ae8c652bc0d3b70574c0a09a7e7f
         }
 
-<<<<<<< HEAD
-//     function uploadFile(inputElement) {
-//   var file = inputElement.files[0];
-//   var reader = new FileReader();
-//   reader.onloadend = function() {
-//     console.log('Encoded Base 64 File String:', reader.result);
-    
-//     /******************* for Binary ***********************/
-//     var data=(reader.result).split(',')[1];
-//      var binaryBlob = atob(data);
-//      addNewProduct.image(binaryBlob);
-//      console.log('Encoded Binary File String:', binaryBlob);
-//   }
-//   reader.readAsDataURL(file);
-// }
-
-    function showForm() {
-
-        return (
-            <div>
-                <Menubar />
-                <div>
-                    <h3>Add a new product :</h3>
-                    <form action="">
-                        ID: <input type="number" placeholder="id?" name="id" value={addNewProduct.id}
-                            onChange={handleChange} />
-                        Title: <input type="text" placeholder="title?" name="title" value={addNewProduct.title}
-                            onChange={handleChange} />
-                        Price: <input type="number" placeholder="price?" name="price" value={addNewProduct.price}
-                            onChange={handleChange} />
-                        <br /><br />
-                        Description: <textarea type="text" placeholder="description?" name="description" value={addNewProduct.description}
-                            onChange={handleChange} />
-                        Category: <input type="text" placeholder="category?" name="category" value={addNewProduct.category}
-                            onChange={handleChange} />
-                        Upload Image: <input type="file" id="image-form" name="image" accept="image/png, image/jpeg" value={addNewProduct.image}
-                            onChange={handleChange} />
-
-                        <br /><br />
-                        {/* Image: <input type="file" class="form-control-file" id="exampleFormControlFile1" /> */}
-                        Rating: <input type="number" placeholder="rate?" name="rating" value={addNewProduct.rating}
-                            onChange={handleChange} />
-                        <button type="submit" onClick={postProduct}>
-                            submit
-                        </button>
-                    </form>
-=======
         function showForm() {
 
 
@@ -271,7 +172,7 @@ function App() {
                             {/* Image: <input type="file" class="form-control-file" id="exampleFormControlFile1" /> */}
                             Rating: <input type="number" placeholder="rate?" name="rating" value={addNewProduct.rating}
                                 onChange={handleChange} />
-                            <button type="submit" onClick={handleOnSubmit}>
+                            <button type="submit" onClick={postProduct}>
                                 submit
                             </button>
                         </form>
@@ -296,7 +197,6 @@ function App() {
                     </Routes>
                 </BrowserRouter>
             </>
->>>>>>> 028bd7249404ae8c652bc0d3b70574c0a09a7e7f
 
         ); // return end
     } // App end
