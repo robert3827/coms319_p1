4e67ee2ff5010dead21cbed66a2b1de00ac423f7
robert3827 <32315287+robert3827@users.noreply.github.com>
2023-12-10 10:43:48 -0600

    add logic to move you to form page when you want to update

--- assignment03_team37/frontend/src/AppFrontend_routing.js
+++ assignment03_team37/frontend/src/AppFrontend_routing.js
@@ -1,7 +1,10 @@
 import { useState, useEffect } from "react";
+import Nav from 'react-bootstrap/Nav';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { Outlet, Link, NavLink } from "react-router-dom";
+
 
 import Form from 'react-bootstrap/Form';
 import FormGroup from 'react-bootstrap/FormGroup'
@@ -13,6 +16,8 @@ import Menubar from "./Menubar";
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Modal from 'react-bootstrap/Modal';
+
 
 
 
@@ -20,8 +25,9 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
 
     const url = "http://localhost:8081/"
-    const [product, setProduct] = useState([]);
-    const [oneProduct, setOneProduct] = useState([]);
+    const [products, setProducts] = useState([]);
+
+
     // new Product
     const [addNewProduct, setAddNewProduct] = useState({
         id: 0,
@@ -32,7 +38,7 @@ function App() {
         ,
         category: ""
         ,
-        image: url + "images",
+        image: url + "images/diglett.png",
         rating: 0.0,
     });
 
@@ -47,7 +53,7 @@ function App() {
             .then((data) => {
                 console.log("Show Catalog of Products :");
                 console.log(data);
-                setProduct(data);
+                setProducts(data);
             });
     }
 
@@ -55,25 +61,26 @@ function App() {
         console.log("Showing Prodcuts");
         return (
             <>
-            <Menubar />
+                <Menubar />
                 <Container>
-                <Row>
-                    {showAllItems()}
-                </Row>
-            </Container>
+                    <Row xs={1} md={2} lg={3} className="g-4">
+                        {showAllItems()}
+                    </Row>
+                </Container>
+
             </>
-            
+
         )
     }
-    
 
-    
+
+
     function showAllItems() {
         return (
-            product.map((el) => (
+            products.map((el) => (
                 <Col sm={6} xs={8} md={4}>
-                    <Card style={{ width: '18rem' }} fluid key={el.id}>
-                        <Card.Img variant="top" src={el.image} />
+                    <Card style={{ width: '18rem', height: '100%' }} fluid key={el.id} className="h-100">
+                        <Card.Img variant="top" src={el.image} style={{ height: '200px', objectFit: 'cover' }} />
                         <Card.Body>
                             <Card.Title>{el.title}</Card.Title>
                             <Card.Text>
@@ -81,138 +88,120 @@ function App() {
                                 Price: $ {el.price} <br />
                                 Rating: {el.rating.rate}
                             </Card.Text>
-                            <Button variant="primary" className="mx-1">Delete</Button>
-                            <Button variant="primary" className="mx-1">Update</Button>
+                            <Button variant="primary" className="mx-1" onClick={() => { handleDelete({ productId: el.id }) }}>Delete</Button>
+                            {/* <Button variant="primary" className="mx-1" onClick={() => { handleUpdate({ product: el }) }}>Update</Button> */}
+                            <NavLink className="btn btn-primary mx-1" to="/form" onClick={() => {handleUpdate({product: el})}}>Update</NavLink>
                         </Card.Body>
                     </Card>
                 </Col>
-        
+
             )
             ));
     }
+    function handleUpdate(props) {
+        console.log("Handle Update");
+        setAddNewProduct(props.product);
+    }
+
+    function handleDelete(props) {
+        console.log("Delete Product: " + props.productId);
+    }
+
+    
 
 
-
-    function getOneProduct(id) {
-        console.log(id);
-        if (id >= 1 && id <= 20) {
-            fetch(url + "getById/" + id)
+        function handleOnSubmit(e) {
+            e.preventDefault();
+            console.log(e.target.value);
+            fetch(url + "create", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify(addNewProduct),
+            })
                 .then((response) => response.json())
                 .then((data) => {
-                    console.log("Show one product :", id);
+                    console.log("Post a new product completed");
                     console.log(data);
-                    setOneProduct(data);
+                    if (data) {
+                        //const keys = Object.keys(data);
+                        const value = Object.values(data);
+                        alert(value);
+                    }
                 });
-        } else {
-            console.log("Wrong number of Product id.");
         }
-    }
-    const showOneItem = oneProduct.map((el) => (
-        <Card style={{ width: '18rem' }} fluid key={el.id}>
-            <Card.Img variant="top" src={el.image}/>
-            <Card.Body>
-                <Card.Title>{el.title}</Card.Title>
-                <Card.Text>
-                    Category:  {el.category} <br />
-                    Price: $ {el.price} <br />
-                    Rating: {el.rating}
-                </Card.Text>
-                <Button variant="primary">Go somewhere</Button>
-            </Card.Body>
-        </Card>
-    ));
 
-    function handleOnSubmit(e) {
-        e.preventDefault();
-        console.log(e.target.value);
-        fetch(url + "create", {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(addNewProduct),
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                console.log("Post a new product completed");
-                console.log(data);
-                if (data) {
-                    //const keys = Object.keys(data);
-                    const value = Object.values(data);
-                    alert(value);
-                }
-            });
-    }
-
-    function handleChange(evt) {
-        const value = evt.target.value;
-        if (evt.target.name === "id") {
-            setAddNewProduct({ ...addNewProduct, id: value });
-        } else if (evt.target.name === "title") {
-            setAddNewProduct({ ...addNewProduct, title: value });
-        } else if (evt.target.name === "price") {
-            setAddNewProduct({ ...addNewProduct, price: value });
-        } else if (evt.target.name === "description") {
-            setAddNewProduct({ ...addNewProduct, description: value });
-        } else if (evt.target.name === "category") {
-            setAddNewProduct({ ...addNewProduct, category: value });
-        } else if (evt.target.name === "image") {
-            setAddNewProduct({ ...addNewProduct, image: value });
-        } else if (evt.target.name === "rating") {
-            setAddNewProduct({ ...addNewProduct, rating: value });
+        function handleChange(evt) {
+            const value = evt.target.value;
+            if (evt.target.name === "id") {
+                setAddNewProduct({ ...addNewProduct, id: value });
+            } else if (evt.target.name === "title") {
+                setAddNewProduct({ ...addNewProduct, title: value });
+            } else if (evt.target.name === "price") {
+                setAddNewProduct({ ...addNewProduct, price: value });
+            } else if (evt.target.name === "description") {
+                setAddNewProduct({ ...addNewProduct, description: value });
+            } else if (evt.target.name === "category") {
+                setAddNewProduct({ ...addNewProduct, category: value });
+            } else if (evt.target.name === "image") {
+                setAddNewProduct({ ...addNewProduct, image: value });
+            } else if (evt.target.name === "rating") {
+                setAddNewProduct({ ...addNewProduct, rating: value });
+            }
         }
-    }
 
-    function showForm() {
+        function showForm() {
+
+
+            return (
+                <div>
+                    <Menubar />
+                    <div>
+                        <h3>Add a new product :</h3>
+                        <form action="">
+                            ID: <input type="number" placeholder="id?" name="id" value={addNewProduct.id}
+                                onChange={handleChange} />
+                            Title: <input type="text" placeholder="title?" name="title" value={addNewProduct.title}
+                                onChange={handleChange} />
+                            Price: <input type="number" placeholder="price?" name="price" value={addNewProduct.price}
+                                onChange={handleChange} />
+                            <br /><br />
+                            Description: <textarea type="text" placeholder="description?" name="description" value={addNewProduct.description}
+                                onChange={handleChange} />
+                            Category: <input type="text" placeholder="category?" name="category" value={addNewProduct.category}
+                                onChange={handleChange} />
+                            Image URL: <textarea type="text" placeholder="image?" name="image" value={addNewProduct.image}
+                                onChange={handleChange} />
+
+                            <br /><br />
+                            {/* Image: <input type="file" class="form-control-file" id="exampleFormControlFile1" /> */}
+                            Rating: <input type="number" placeholder="rate?" name="rating" value={addNewProduct.rating}
+                                onChange={handleChange} />
+                            <button type="submit" onClick={handleOnSubmit}>
+                                submit
+                            </button>
+                        </form>
+
+
+                    </div>
+                </div>
+            )
+        }
+
+
+
+
 
         return (
-            <div>
-                <Menubar />
-                <div>
-                    <h3>Add a new product :</h3>
-                    <form action="">
-                        ID: <input type="number" placeholder="id?" name="id" value={addNewProduct.id}
-                            onChange={handleChange} />
-                        Title: <input type="text" placeholder="title?" name="title" value={addNewProduct.title}
-                            onChange={handleChange} />
-                        Price: <input type="number" placeholder="price?" name="price" value={addNewProduct.price}
-                            onChange={handleChange} />
-                        <br /><br />
-                        Description: <textarea type="text" placeholder="description?" name="description" value={addNewProduct.description}
-                            onChange={handleChange} />
-                        Category: <input type="text" placeholder="category?" name="category" value={addNewProduct.category}
-                            onChange={handleChange} />
-                        Image URL: <textarea type="text" placeholder="image?" name="image" value={addNewProduct.image}
-                            onChange={handleChange} />
+            <>
 
-                        <br /><br />
-                        {/* Image: <input type="file" class="form-control-file" id="exampleFormControlFile1" /> */}
-                        Rating: <input type="number" placeholder="rate?" name="rating" value={addNewProduct.rating}
-                            onChange={handleChange} />
-                        <button type="submit" onClick={handleOnSubmit}>
-                            submit
-                        </button>
-                    </form>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<div>{showAllProducts()}</div>} />
+                        <Route path="/form" element={<div>{showForm({})}</div>} />
+                    </Routes>
+                </BrowserRouter>
+            </>
 
-                    
-                </div>
-            </div>
-        )
-    }
-
-
-
-
-
-    return (
-        <>
-
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<div>{showAllProducts()}</div> } />
-                    <Route path="/form" element={<div>{showForm()}</div>} />
-                </Routes>
-            </BrowserRouter>
-        </>
-
-    ); // return end
-} // App end
-export default App;
+        ); // return end
+    } // App end
+    export default App;
