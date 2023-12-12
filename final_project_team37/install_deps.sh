139ca96b1fa73c003d1737aa52800bf14fe9ced3
robert3827 <32315287+robert3827@users.noreply.github.com>
2023-12-12 11:05:28 -0600

    got dependencies

--- final_project_team37/install_deps.sh
+++ final_project_team37/install_deps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 echo "Starting installs"
-ls -lah
 
 # Go to backend folder and run scripts
 cd backend && npm install cors express nodemon mongodb 
 
 
 # Go to frontend folder and run scripts
-cd frontend && npm install bootstrap bootstrap-react axios react-router-dom
+cd frontend && npm install bootstrap bootstrap-react axios react-router-dom react-toastify
+
 
 # $SHELL
